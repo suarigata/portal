d3306b90326a26b6c0ea52350c9c4ff54f10076d
thiago <thiago@TarnVeddra>
2012-11-16 03:19:50 -0200

    ntregas com Endereço corrigido

--- app/Model/Entrega.php
+++ app/Model/Entrega.php
@@ -3,31 +3,31 @@ class Entrega extends AppModel{
 	
 	// calcular frete e prazo
 	public function calculaCusto($cepDest, $transportadora, $produtos){
-		$client = new SoapClient("http://localhost:8090/grupo9/webservice/ws.php?wsdl");
+		$client = new SoapClient("http://localhost:8000/grupo9/webservice/ws.php?wsdl");
 		$result = $client->calculaFreteEPrazo(03180100, $cepDest, $transportadora, $produtos);
 		return $result;
 	}
 	
 	public function listaTransportadoras($nome = NULL){
-		$client = new SoapClient("http://localhost:8090/grupo9/webservice/ws.php?wsdl");
+		$client = new SoapClient("http://localhost:8000/grupo9/webservice/ws.php?wsdl");
 		$result = $client->listarTransportadoras($nome);
 		return $result;
 	}
 	
 	public function novaEntrega($cepDest, $transportadora, $produtos){
-		$client = new SoapClient("http://localhost:8090/grupo9/webservice/ws.php?wsdl");
+		$client = new SoapClient("http://localhost:8000/grupo9/webservice/ws.php?wsdl");
 		$result = $client->cadastrarEntrega(10, 03180100, $cepDest, $transportadora, $produtos);
 		return $result;
 	}
 	
 	public function cancelarEntrega($entrega){
-		$client = new SoapClient("http://localhost:8090/grupo9/webservice/ws.php?wsdl");
+		$client = new SoapClient("http://localhost:8000/grupo9/webservice/ws.php?wsdl");
 		$result = $client->cancelarEntrega($entrega);
 		return $result;
 	}
 	
 	public function consultarEntrega($entrega){
-		$client = new SoapClient("http://localhost:8090/grupo9/webservice/ws.php?wsdl");
+		$client = new SoapClient("http://localhost:8000/grupo9/webservice/ws.php?wsdl");
 		$result = $client->consultarEntrega($entrega);
 		return $result;
 	}
