e26046713c33c09399675bf86950133933100d9c
Alex <alex@mailinator.com>
2012-11-16 00:56:35 -0200

    Aplicando layout.

---
+++ app/Controller/ValidacaoCreditosController.php
@@ -0,0 +1,8 @@
+<?php
+class ValidacaoCreditosController extends AppController{
+	
+	public function index(){
+		$this->set('validar', $this->ValidacaoCredito->validarCredito("39715732828"));
+	}
+}
+?>
