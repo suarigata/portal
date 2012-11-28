9c0030c7c262e206207d7af096866fea9eb0b115
thiago <thiago@TarnVeddra>
2012-11-28 01:31:49 -0200

    Issue 1 resolvido

--- app/Controller/ValidacaoCreditosController.php
+++ app/Controller/ValidacaoCreditosController.php
@@ -6,24 +6,31 @@ class ValidacaoCreditosController extends AppController{
 	}
 	
 	public function escolheFormaPagamento () {
-		$scores = array('A' => 30000, 'B' => 15000, 'C' => 10000, 'D' => 5000, 'X' => 0);
-	
-		$valorDaCompra=CakeSession::read('valorDaCompra');
 		$cliente=CakeSession::read('cliente');
-		$valido = $this->ValidacaoCredito->validarCredito($cliente['cpf']);
-	
-		if($valido['return']['error'] == ""){
-			$this->loadModel('Cartao');
-				
-			$cartoes = $this->Cartao->consultarBandeiras();
-				
-			if ($scores[$valido['return']['score']] >= $valorDaCompra)
-				$cartoes['BOLETO'] = 'Boleto';
-			else
-				$cartoes = array('Boleto');
+		if ($cliente != "") {	
+			$scores = array('A' => 30000, 'B' => 15000, 'C' => 10000, 'D' => 5000, 'X' => 0);
+		
+			$valorDaCompra=CakeSession::read('valorDaCompra');
+			
+			$valido = $this->ValidacaoCredito->validarCredito($cliente['cpf']);
+		
+			if($valido['return']['error'] == ""){
+				$this->loadModel('Cartao');
+					
+				$cartoes = $this->Cartao->consultarBandeiras();
+					
+				if ($scores[$valido['return']['score']] >= $valorDaCompra)
+					$cartoes['BOLETO'] = 'Boleto';
+				else
+					$cartoes = array('Boleto');
+			}
+			$this->set('pagamentos', $cartoes);
+			$this->set('total', $valorDaCompra);
 		}
-		$this->set('pagamentos', $cartoes);
-		$this->set('total', $valorDaCompra);
+		else {
+			$this->Session->setFlash('Você não está logado, por favor realize login para continuar');
+			$this->redirect(array('controller' => 'Carrinho', 'action' => 'listCarrinho'));
+		}
 	}
 }
 ?>
